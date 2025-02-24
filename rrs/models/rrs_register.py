34ce1d03da293f351adc36e7045a662dd640605b
Mariia <m.bystramovich@gmail.com>
2025-02-24 10:29:22 +0200

    upd for v2, remove unused fields

--- rrs/models/rrs_register.py
+++ rrs/models/rrs_register.py
@@ -7,18 +7,9 @@ class RRSRegister(models.Model):
 
     address = fields.Char(string="Subscription controller address in Robonomics parachain", required=True)
     customer_email = fields.Char(string="Email")
-    status = fields.Many2one(
-        comodel_name="rrs.register.status",
-        string="Status",
-        help="Status indicates wheter the subscribtion was bought, not paid or expired",
-    )
-    started_date = fields.Datetime()
-    expired_date = fields.Datetime()
+    paid = fields.Boolean(string="Is a service paid or free", default=False)
+    last_paid = fields.Datetime()
     pinata_key = fields.Char(string="Pinata Key")
     pinata_secret = fields.Char(string="Pinata Secret")
-    subscription = fields.Boolean()
-    owner_address = fields.Char(
-        string="Owner address",
-        required=True,
-        default="123"
-    )
+    revolut_cid = fields.Char(string="Revolut customer id")
+    revolut_order_id = fields.Char(string="Revolut last order id")
